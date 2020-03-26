15d4036be71748095405039347bab8ee972553a1
mamolenaar <36441439+mamolenaar@users.noreply.github.com>
2020-03-26 13:30:34 +0100

    Update load_data.py

--- hn/load_data.py
+++ hn/load_data.py
@@ -4,6 +4,6 @@ import os
 
 def load_data():
     this_directory = os.path.dirname(os.path.abspath(__file__))
-    data = pd.read_csv(os.path.join(this_directory, 'HN_radiomicFeatures.csv'))
+    data = pd.read_csv(os.path.join(this_directory, 'HN_radiomicFeatures.csv'), index_col=0)
 
     return data
